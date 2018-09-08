b5803af69ec723500f03da5ac1d7414b8def8bd2
belqati <nahnahgames@gmail.com>
2018-09-08 15:57:50 -0500

    add local storage functionality for ES5

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,7 +12,7 @@ function Book(author, year, title, isbn){
 function UI(){}
 
 // UI PROTOTYPES
-// addBook
+// add book to UI
 UI.prototype.addBookToList = function(book){
   const list = document.querySelector('#book-list');
   // create row element
@@ -66,7 +66,78 @@ UI.prototype.showAlert = function(message, className){
   }, 2000);
 }
 
+// Local Storage Constructor for holding prototypes; used by each instance of new Book
+function Store(){}
+
+// get books from localStorage
+Store.prototype.getBooks = function(){
+  let books;
+  if(localStorage.getItem('books') === null){
+    // create empty array for adding first book
+    books = [];
+  } else {
+    // convert localStorage string to array
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  // return empty/parsed array
+  return books;
+}
+
+// display books from localStorage in UI
+Store.prototype.displayBooks = function(){
+  // instantiate Store for prototype access
+  const store = new Store();
+  // get books method for grabbing array
+  const books = store.getBooks();
+  // loop through array
+  books.forEach(function(book){
+    // instantiate UI to use its methods
+    const ui = new UI;
+    // add book to UI
+    ui.addBookToList(book);
+  });
+}
+
+// add book to local storage
+Store.prototype.addBook = function(book){
+  // instantiate Store for prototype access
+  const store = new Store();
+  // get books method for grabbing array
+  const books = store.getBooks();
+  // add new book object to that array
+  books.push(book);
+  // set updated array into localStorage as a string
+  localStorage.setItem('books', JSON.stringify(books))
+}
+
+// remove book from local storage
+Store.prototype.removeBook = function(isbn){
+  // instantiate Store for prototype access
+  const store = new Store();
+  // get books method for grabbing array
+  const books = store.getBooks();
+
+  // loop through and update
+  books.forEach(function(book, index){
+    // if isbn matches, splice book obj out of array
+    if(book.isbn === isbn){
+      books.splice(index, 1);
+    }
+  });
+
+  // update localStorage via stringify
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
 // EVENT LISTENERS
+// DOM Load Event for populating UI with books from localStorage
+document.addEventListener('DOMContentLoaded', function(){
+  // instantiate Store for prototype access
+  const store = new Store();
+  // display books from local storage
+  store.displayBooks();
+});
+
 // Event listener to form for add book
 document.querySelector('#book-form').addEventListener('submit', function(e){
   // get form values
@@ -81,6 +152,9 @@ document.querySelector('#book-form').addEventListener('submit', function(e){
   // instantiate UI for prototype access
   const ui = new UI();
 
+  // instantiate Store for prototype access
+  const store = new Store();
+
   // validate form input
   if(author === '' || year === '' || title === '' || isbn === ''){
     // show error alert
@@ -88,6 +162,9 @@ document.querySelector('#book-form').addEventListener('submit', function(e){
   } else {
   // add book to list
   ui.addBookToList(book);
+
+  // add book to localStorage
+  store.addBook(book);
 
   // show success alert
   ui.showAlert('Book successfully added!', 'success');
@@ -104,11 +181,15 @@ document.querySelector('#book-form').addEventListener('submit', function(e){
 document.querySelector('#book-list').addEventListener('click', function(e){
   // instantiate UI for prototype access
   const ui = new UI();
-  // delete book
+  // instantiate Store for prototype access
+  const store = new Store();
+  // delete book from UI
   ui.deleteBook(e.target);
+  // delete book from localStorage
+  store.removeBook(e.target.parentElement.parentElement.previousElementSibling.textContent);
   // show success alert
   ui.showAlert('Book removed!', 'success');
 
-  // Brad included the following line--it is unnecessary
-  // e.preventDefault();
+  // prevent default behavior for delete (else UI jumps to top of page on delete via href="#")
+  e.preventDefault();
 });
